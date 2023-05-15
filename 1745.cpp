ae5d6e03dfdf7ae9118f79a0dd4202c9a6af4dc6
AgNO3 <33011851+hhhxiao@users.noreply.github.com>
2023-05-15 15:03:46 +0800

    new dbg.h(simpler)

--- 1745.cpp
+++ 1745.cpp
@@ -4,21 +4,50 @@
 
 using namespace std;
 
+std::unordered_map<int, bool> cache;
+
 bool is(const std::string& s, int b, int e) {
+    auto idx = b * 10000 + e;
+    if (cache.count(idx)) {
+        return cache[idx];
+    }
+
     int len = (e - b);
+    if (len == 0) return false;
     if (len == 1) return true;
     for (int i = 0; i <= len / 2; i++) {
-        if (s[b + i] == s[e - i - 1]) {
-            return true;
+        if (s[b + i] != s[e - i - 1]) {
+            cache[idx] = false;
+
+            return false;
         }
     }
-    return false;
+    cache[idx] = true;
+    return true;
 }
 
 bool find(const string& s, int start, int part) {
     if (start >= s.size()) return false;
     if (part == 1) {
+        return is(s, start, s.size());
     }
+
+    int p = start + 1;
+    while (p < s.size()) {
+        if (s[p - 1] == s[start] && is(s, start, p) && find(s, p, part - 1)) {
+            return true;
+        }
+        p++;
+    }
+
+    return false;
 }
 
-bool checkPartitioning(string s) { dbg(1); }
+bool checkPartitioning(string s) { return find(s, 0, 3); }
+
+int main() {
+    dbg(checkPartitioning(
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+    return 0;
+}
